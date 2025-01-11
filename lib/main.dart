4ee34219240c77e5ad8bcffb382dd9e69d6db57e
gurasan0110 <gurasan0110@icloud.com>
2025-01-12 05:50:10 +0900

    スプラッシュ画面を設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:search_qiita_articles/app.dart';
 
-void main() => runApp(const App());
+void main() {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  FlutterNativeSplash.remove();
+
+  runApp(const App());
+}
