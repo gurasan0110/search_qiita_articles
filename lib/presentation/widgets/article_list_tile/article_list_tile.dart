d18ab093c0192cd240eaa4983eba1276ea813ae1
gurasan0110 <gurasan0110@icloud.com>
2025-01-20 23:37:12 +0900

    urlを開く

--- lib/presentation/widgets/article_list_tile/article_list_tile.dart
+++ lib/presentation/widgets/article_list_tile/article_list_tile.dart
@@ -4,6 +4,7 @@ import 'package:search_qiita_articles/presentation/widgets/article_list_tile/art
 import 'package:search_qiita_articles/presentation/widgets/article_list_tile/article_list_tile_header.dart';
 import 'package:search_qiita_articles/presentation/widgets/defaults/d_text.dart';
 import 'package:search_qiita_articles/presentation/widgets/tag_chips/tag_chips.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ArticleListTile extends StatelessWidget {
   const ArticleListTile(this.article, {super.key});
@@ -12,22 +13,25 @@ class ArticleListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      minimum: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 8,
-        children: [
-          ArticleListTileHeader(article),
-          DText(
-            article.title,
-            size: 16,
-            weight: FontWeight.bold,
-            maxLines: 3,
-          ),
-          TagChips(article.tags),
-          ArticleListTileBottom(article),
-        ],
+    return InkWell(
+      onTap: () => launchUrl(article.url),
+      child: SafeArea(
+        minimum: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 8,
+          children: [
+            ArticleListTileHeader(article),
+            DText(
+              article.title,
+              size: 16,
+              weight: FontWeight.bold,
+              maxLines: 3,
+            ),
+            TagChips(article.tags),
+            ArticleListTileBottom(article),
+          ],
+        ),
       ),
     );
   }
